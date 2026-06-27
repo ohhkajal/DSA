d0ef8ef4dcb8a14bdbd12fe7cc99e4cdbbc49104
Kajal Nayak <159150254+ohhkajal@users.noreply.github.com>
2026-06-27 13:42:41 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.58%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
+        int bestending=a[0];
+        int ans=a[0];
+        for(int i=1;i<n;i++){
+            int v1=bestending+a[i];
+            int v2=a[i];
+            bestending=max(v1,v2);
+            ans=max(bestending,ans);
+        }
+        return ans;
+
+        
+    }
+};
