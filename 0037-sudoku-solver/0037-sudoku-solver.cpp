1706cc7ee6eb8dbd21465581627a251f269dc587
Kajal Nayak <159150254+ohhkajal@users.noreply.github.com>
2026-07-01 20:29:47 +0530

    Time: 375 ms (44.15%), Space: 8.7 MB (71.24%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num)
+                return false;
+            if (board[i][col] == num)
+                return false;
+            int r = 3 * (row / 3) + i/ 3;
+            int c = 3 * (col / 3) + i% 3;
+            if (board[r][c] == num)
+                return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(board,i,j,ch)){
+                            board[i][j]=ch;
+                            if(solve(board))
+                            return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
+    }
+        
+     void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+     }
+};
