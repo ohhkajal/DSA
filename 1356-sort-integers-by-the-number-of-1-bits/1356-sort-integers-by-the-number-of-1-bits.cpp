8c8e6cc3b0da879c8700ca44c870b0b5a2c2d008
Kajal Nayak <kajalnayak507@gmail.com>
2026-07-22 13:40:05 +0530

    Time: 3 ms (47.19%), Space: 14.3 MB (32.04%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            int x = __builtin_popcount(a);
+            int y = __builtin_popcount(b);
+
+            if (x == y)
+                return a < b;
+
+            return x < y;
+        });
+
+        return arr;
+    }
+};
